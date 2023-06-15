f33be17eeca224e2bcaa08f73c0633ede7650e83
mrpapus <124309793+mrpapus@users.noreply.github.com>
2023-06-15 14:34:44 -0600

    Update pyhton.py

---
+++ pyhton.py
@@ -0,0 +1,8 @@
+num1X = float(input("1X = "))
+num1Y = float(input("1Y = "))
+num2X = float(input("2X = "))
+num2Y = float(input("2Y = "))
+
+distance = ((num2X -num1X)**2+ (num2Y - num1Y)**2)**0.5                            
+
+print("distance is ",distance)
